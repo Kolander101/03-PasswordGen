796bbf5b022883c0581ea8f70cc0e9aec2db4e88
Michael Kolander <michaelkolander@Michaels-MBP-4.lan>
2022-03-31 19:41:33 -0500

    create variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var UCletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lcletters = "abcdefghijklmnopqrstuvwxyz";
+var num = "0123456789";
+var sc ="#$%&'()*+,-./:;<=>?@[]^_`{|";
+var chalength = 8;
+var all ="";
+var generateBtn = document.querySelector("#generate")
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +18,15 @@ function writePassword() {
 
 }
 
+function Prompt{
+  chalength= window.prompt("How many characters would you like your password to be?(Min 8-128 characters)"));
+  if (chalength < 8 || chalength > 128){
+    alert("Length of characters must be a digit between 8-128. Try again");
+  }
+
+  if (confirm("Would you like Uppercase letters in your password?"))
+  var UCletters= UCletters.concat
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
